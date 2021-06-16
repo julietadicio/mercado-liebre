2714ee65d2e1608c2125a28903ca521def7cf05f
julietadicio <julietadicio@gmail.com>
2021-06-15 21:51:56 -0300

    Forms y procfile

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, function() {
     console.log('Servidor esta corriendo en el puerto 4000');
-})
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
 })
